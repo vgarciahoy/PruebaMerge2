ff831ffb405d64ded58988a79bcb4d182a2d5589
vgarciahoy <vgarciahoyas@deloitte.es>
2017-09-07 08:29:16 +0200

    //Cambio en desarrollo 8:29

---
+++ SegundaPrueba/SegundaPrueba/Prueba2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundaPrueba
+{
+    class Prueba2
+    {
+        //Cambio en desarrollo 8:29
+    }
+}
